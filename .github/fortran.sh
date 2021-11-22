d1bfa31239300ea810e14a5dc794cc0e4175b46c
Bela Pecsek <bela.pecsek@gmail.com>
2021-11-22 21:51:00 +0100

    gfortran-11 added

--- .github/fortran.sh
+++ .github/fortran.sh
@@ -3,6 +3,7 @@
 gfortran-9 --version
 gfortran-10 --version
 sudo apt-get install -y gcc-11
+sudo apt-get install -y gfortran-11
 gfortran-11 --version
 #sudo apt-get install -y flang libgmp-dev
 #flang --version
