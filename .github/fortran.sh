b255edd0d564c2d19f86b35f62b3f21492b2364c
Bela Pecsek <bela.pecsek@gmail.com>
2021-11-30 17:23:16 +0100

    playing with flang

--- .github/fortran.sh
+++ .github/fortran.sh
@@ -5,6 +5,5 @@ gfortran-10 --version
 sudo apt-get install -y gcc-11
 sudo apt-get install -y gfortran-11
 gfortran-11 --version
-clang --version
-sudo apt-get install -y flang libgmp-dev libomp-11-dev
+sudo apt-get install -y flang libgmp-dev
 flang --version
