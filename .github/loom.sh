1e08b874b235b1694b056c0865c7c5e8648dc64b
Bela Pecsek <50743731+bpecsek@users.noreply.github.com>
2021-11-30 13:37:21 +0100

    Update loom.sh

--- .github/loom.sh
+++ .github/loom.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-wget https://download.java.net/java/early_access/loom/6/openjdk-18-loom+5-282_linux-x64_bin.tar.gz
+wget https://download.java.net/java/early_access/loom/6/openjdk-18-loom+6-282_linux-x64_bin.tar.gz
 tar -xvf openjdk-18-loom+6-282_linux-x64_bin.tar.gz
 sudo ln -sf $PWD/jdk-18/bin/javac /usr/bin/loomjavac
 sudo ln -sf $PWD/jdk-18/bin/java /usr/bin/loomjava
